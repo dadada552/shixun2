e38e55e1839df65653fde027f33571dcd52edb7d
dadada552 <510242740@qq.com>
2022-09-20 22:04:53 +0800

    梦雪谷

---
+++ education-app/App.vue
@@ -0,0 +1,21 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	@import url("common/css/common.css");
+	@import url('static/icon/teacth/iconfont.css');
+	@import url('static/icon/play/iconfont.css');
+	@import url('static/icon/gotop/iconfont.css');
+</style>
---
+++ education-app/api/api.js
@@ -0,0 +1,51 @@
+import http from './http'
+/** 获取轮播图数据*/
+const getbanner = () => {
+  let data = http.get('article/api/advert/show/1')
+  return data
+}
+/** 获取首页热门推荐分类*/
+const getHotrecom = ()=>{
+	let data = http.get('article/api/category/label/list')
+	return data
+}
+/** 获取热门数据 */
+const getHotList = (msg)=>{
+	let data = http.post('course/api/course/search',msg)
+	return data
+}
+/**获取近期上新数据*/
+const getNewList = (msg)=>{
+	let data = http.post('course/api/course/search',msg)
+	return data
+}
+/**获取免费精选数据*/
+const getFreeList = (msg)=>{
+	let data = http.post('course/api/course/search',msg)
+	return data
+}
+/**获取付费精品*/
+const getNiceList = (msg)=>{
+	let data = http.post('course/api/course/search',msg)
+	return data
+}
+/**获取阅读数据*/
+const getReadList = (msg)=>{
+	let data = http.post('article/api/article/search',msg)
+	return data
+}
+/**获取热门回答*/
+const getHotquestionList = (msg)=>{
+	let data = http.post('question/api/question/hot',msg)
+	return data
+}
+export {
+	getbanner,
+	getHotrecom,
+	getHotList,
+	getNewList,
+	getFreeList,
+	getNiceList,
+	getReadList,
+	getHotquestionList
+}
